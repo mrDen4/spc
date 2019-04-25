4dbbbc3665690face712f68d9dfc0c75b21f2e89
Denis Davydov <den.saint.k@gmail.com>
2019-04-25 16:33:21 +0300

    add 3 pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
-    $('.header__menu').on('click', function () {
-        $('.header__menu-list').toggleClass('header__menu-list--active');
-        $('.menu__img--open').toggleClass('menu__img--open--active');
-        $('.menu__img--exit').toggleClass('menu__img--exit--active');
-        $('.bg').toggleClass('bg--active');
-    });
-    
-    $('.bg').on('click', function () {
-        $('.header__menu-list').removeClass('header__menu-list--active');
-        $('.menu__img--exit').toggleClass('menu__img--exit--active');
-        $('.menu__img--open').toggleClass('menu__img--open--active');
-        $('.bg').removeClass('bg--active');
+    $('.content__slider').slick({
+        prevArrow: $('.btns__btn--l'),
+        nextArrow: $('.btns__btn--r')
     });
 });
