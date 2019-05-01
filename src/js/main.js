f20cda312e28835e376de3705f99a991033409e3
Denis Davydov <den.saint.k@gmail.com>
2019-05-02 00:29:03 +0300

    finish desktop vers

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
         prevArrow: $('.btns__btn--l'),
         nextArrow: $('.btns__btn--r')
     });
+
+    $('.hero .hero__btn').on('click', function () {
+        $('.popup').toggleClass('popup--active');
+        $('.bg').toggleClass('bg--active');
+    });
+
+    $('.bg, .popup .popup__exit').on('click', function () {
+        $('.popup').removeClass('popup--active');
+        $('.bg').removeClass('bg--active');
+    });
 });
